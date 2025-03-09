3a4a932d03ac16b4c67855ccdbe915c0097c1400
Nils Homer <nilshomer@gmail.com>
2025-03-08 23:40:58 -0700

    fix

--- .github/scripts/update-docs.sh
+++ .github/scripts/update-docs.sh
@@ -3,8 +3,7 @@
 set -euo pipefail
 
 echo -e "<!-- start usage -->\n\`\`\`console\n" > usage.txt;
-# the `sed` is needed since the # of cores detected may differ across environments
-./target/debug/fqgrep --help | ./target/debug/fqgrep --help | sed -e '\/[default: [0-9]*\]/ s/\[default: [0-9]*\]/[default: 10]/' >> usage.txt;
+./target/debug/fqtk demux --help | sed -e 's_^[ ]*$__g' >> usage.txt
 echo -e "\`\`\`\n<!-- end usage -->" >> usage.txt;
 sed -e '/<!-- start usage -->/,/<!-- end usage -->/!b' -e '/<!-- end usage -->/!d;r usage.txt' -e 'd' README.md > README.md.new;
 mv README.md.new README.md;
